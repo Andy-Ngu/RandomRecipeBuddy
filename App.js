74205ed5cad745ef494d7e07a5c7fb4900b6a97b
Andy <andy_nguyen@csu.fullerton.edu>
2021-02-15 18:37:40 -0800

    Updated App.js and added placeholder for contact screen

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen.js';
 import ResultsScreen from './src/screens/ResultsScreen.js';
+import AboutScreen from './src/screens/AboutScreen.js';
+import ContactScreen from './src/screens/ContactScreen.js';
 
 const navigator = createStackNavigator(
     {
     Search: SearchScreen,
-    Results: ResultsScreen
+    Results: ResultsScreen,
+    About: AboutScreen,
+    Contact: ContactScreen,
     },
   {
   initialRouteName: 'Search',
---
+++ src/screens/ContactScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Image, View, Text} from 'react-native';
+
+const ContactScreen = ({navigation}) => {
+    return (
+        <SafeAreaView>
+            <Text>Contact</Text>
+        </SafeAreaView>
+        );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default ContactScreen;
